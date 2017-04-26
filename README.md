a17785e163afccabad1379725d43dbb79d23a2be
Abhi Agarwal <me@abhiagarwal.com>
2017-04-25 22:44:34 -0400

    begginning work with getting outlook email

---
+++ README.md
@@ -0,0 +1,5 @@
+### Outlook
+
+- Get emails:
+
+GET `https://outlook.office.com/api/v2.0/me/MailFolders/sentitems/messages/?$select=Sender,Subject&$search="from:abhi@newsai.co"`
--- mail.go
+++ mail.go
@@ -59,6 +59,23 @@ type EmailRequestAttachment struct {
 	} `json:"Message"`
 }
 
+type GetEmailRequest struct {
+	OdataContext string `json:"@odata.context"`
+	Value        []struct {
+		OdataID   string `json:"@odata.id"`
+		OdataEtag string `json:"@odata.etag"`
+		ID        string `json:"Id"`
+		Subject   string `json:"Subject"`
+		Sender    struct {
+			EmailAddress struct {
+				Name    string `json:"Name"`
+				Address string `json:"Address"`
+			} `json:"EmailAddress"`
+		} `json:"Sender"`
+		OdataType string `json:"@odata.type,omitempty"`
+	} `json:"value"`
+}
+
 func (o *Outlook) SendEmail(c context.Context, from string, to string, subject string, body string, email models.Email) error {
 	if len(o.AccessToken) > 0 {
 		contextWithTimeout, _ := context.WithTimeout(c, time.Second*15)
@@ -96,6 +113,7 @@ func (o *Outlook) SendEmail(c context.Context, from string, to string, subject s
 		}
 
 		if response.StatusCode == 200 || response.StatusCode == 202 {
+			log.Infof(c, "%v", response.Body)
 			return nil
 		}
 
@@ -188,4 +206,21 @@ func (o *Outlook) SendEmailWithAttachments(r *http.Request, c context.Context, f
 	}
 
 	return errors.New("No access token supplied")
+}
+
+func (o *Outlook) GetEmail(r *http.Request, c context.Context, to string, subject string) error {
+	if len(o.AccessToken) > 0 {
+		URL := BASEURL + "api/v2.0/me/MailFolders/sentitems/messages/?$select=Sender,Subject&$search=\"subject:" + subject + "\""
+		req, _ := http.NewRequest("GET", URL, nil)
+
+		req.Header.Add("Authorization", "Bearer "+o.AccessToken)
+		req.Header.Add("Content-Type", "application/json")
+
+		response, err := client.Do(req)
+		if err != nil {
+			log.Errorf(c, "%v", err)
+			return err
+		}
+	}
+	return nil
 }
